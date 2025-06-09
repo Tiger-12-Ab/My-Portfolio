6a03d4e3db9ddfd25ffacf1677a138df6cf0a97c
tiger <tiger.12.ht@gmail.com>
2025-06-09 01:12:55 -0700

    Fix: Installed react-slick and slick-carousel

--- src/components/ProjectHighlights.js
+++ src/components/ProjectHighlights.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabase";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const ProjectHighlights = () => {
   const [projects, setProjects] = useState([]);
